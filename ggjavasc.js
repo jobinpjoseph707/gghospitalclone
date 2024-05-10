752b8bd26d9dcf7c08d37b0321edf0c09d241490
jobinpjoseph707 <jobinpjoseph700@gmail.com>
2024-05-11 01:12:12 +0530

    -inital commit

---
+++ ggjavasc.js
@@ -0,0 +1,15 @@
+const carousel = document.getElementById('carouselSlide1');
+        const carouselInstance1 = new bootstrap.Carousel(carousel);
+        setTimeout(function() {
+            carouselInstance.cycle();
+        }, 4000);
+const carouse2 = document.getElementById('carouselSlide2');
+        const carouselInstance2 = new bootstrap.Carousel(carousel);
+        setTimeout(function() {
+            carouselInstance.cycle();
+        }, 4000);
+const carouse3 = document.getElementById('carouselSlide3');
+    const carouselInstance3 = new bootstrap.Carousel(carousel);
+        setTimeout(function() {
+            carouselInstance.cycle();
+        }, 4000);
